3162d7b24614b0dbafcaa48d064ca0701722717c
Fong Say Kiat <65012113+fsk1997@users.noreply.github.com>
2024-02-17 11:30:13 +0800

    Convert PrismicDom to use prismic client

--- src/prismic.ts
+++ src/prismic.ts
@@ -1,12 +1,11 @@
-import Prismic from '@prismicio/client';
+import * as prismic from "@prismicio/client";
 const API_ENDPOINT = "https://astro-prismic-demo.prismic.io/api/v2";
 
-const client = Prismic.client(API_ENDPOINT);
+const client = prismic.createClient(API_ENDPOINT);
 
 export async function getAllPosts() {
-    return client.query([Prismic.predicates.at('document.type', 'blog_post')]);
+    return client.getAllByType('blog_post');
 }
-
 
 export async function getPage(uuid: string) {
     return client.getByUID('page', uuid, { pageSize: 1, page: 1 });
